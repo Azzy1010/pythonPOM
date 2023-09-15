5548bc636538b96a47c6f9affbad1ea7bdd27c29
Azzy1010 <azfergul1010@gmail.com>
2023-09-15 15:17:27 +0500

    Update config file to add the ss in the same directory

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -11,7 +11,9 @@ f_date = date.strftime('%d-%m-%Y')
 def ss(driver, value):
     global item
     item = value
-    driver.save_screenshot(f"C:\D--/Reports/{f_date}\SS/{item}.png")
+    SS_directory = f"./Reports/{f_date}/SS"
+    os.makedirs(SS_directory, exist_ok=True)
+    driver.save_screenshot(f"{SS_directory}/{item}.png")
     return item
 
 
@@ -21,10 +23,12 @@ date = datetime.date.today()
 @pytest.hookimpl(tryfirst=True)
 def pytest_configure(config):
     # check if the the folder to save reports is present or not, if not then it will create the folder
-    if not os.path.exists(f'C:\D--/Reports/{f_date}/SS'):
-        os.makedirs(f'C:\D--/Reports/{f_date}/SS')
+    # if not os.path.exists(f'C:\D--/Reports/{f_date}/SS'):
+    #     os.makedirs(f'C:\D--/Reports/{f_date}/SS')
     # Change the name of the HTML report
-    config.option.htmlpath = f'C:\D--/Reports/{f_date}/' + 'Automation_reports ' + f_date + ".html"
+    report = f"./Reports/{f_date}/"
+    os.makedirs(report, exist_ok=True)
+    config.option.htmlpath = f'{report}' + 'Automation_reports ' + f_date + ".html"
 
 
 @pytest.hookimpl(hookwrapper=True)
@@ -33,7 +37,6 @@ def pytest_runtest_makereport():
     report = outcome.get_result()
     if report.when == "call" and report.failed:
         report.extra = [extras.image(f"C:\D--/Reports/{f_date}/SS/{item}.png", "Screenshot")]
-        # report.extra = [extras.image(("/",f"{item}.png"), "Screenshot")]
 
 
 def pytest_html_report_title(report):
