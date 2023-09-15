f99fcd098c53b4d47b0993533ee48318ab7f5e3a
Azzy1010 <azfergul1010@gmail.com>
2023-09-15 17:03:09 +0500

    conftest updated

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -3,18 +3,19 @@ import os
 import pytest
 from pytest_html import extras
 import datetime
+from pathlib import Path
 
 date = datetime.date.today()
 f_date = date.strftime('%d-%m-%Y')
 
 
 def ss(driver, value):
-    global item
+    global item,ss_directory
     item = value
-    SS_directory = f"./Reports/{f_date}/SS"
-    os.makedirs(SS_directory, exist_ok=True)
-    driver.save_screenshot(f"{SS_directory}/{item}.png")
-    return item
+    ss_directory = f"./Reports/{f_date}/SS"
+    os.makedirs(ss_directory, exist_ok=True)
+    driver.save_screenshot(f"{ss_directory}/{item}.png")
+    return item,ss_directory
 
 
 date = datetime.date.today()
@@ -22,10 +23,6 @@ date = datetime.date.today()
 
 @pytest.hookimpl(tryfirst=True)
 def pytest_configure(config):
-    # check if the the folder to save reports is present or not, if not then it will create the folder
-    # if not os.path.exists(f'C:\D--/Reports/{f_date}/SS'):
-    #     os.makedirs(f'C:\D--/Reports/{f_date}/SS')
-    # Change the name of the HTML report
     report = f"./Reports/{f_date}/"
     os.makedirs(report, exist_ok=True)
     config.option.htmlpath = f'{report}' + 'Automation_reports ' + f_date + ".html"
@@ -35,8 +32,9 @@ def pytest_configure(config):
 def pytest_runtest_makereport():
     outcome = yield
     report = outcome.get_result()
+    main = str(Path.cwd())
     if report.when == "call" and report.failed:
-        report.extra = [extras.image(f"C:\D--/Reports/{f_date}/SS/{item}.png", "Screenshot")]
+        report.extra = [extras.image(f"{main}/Reports/{f_date}/SS/{item}.png", "Screenshot")]
 
 
 def pytest_html_report_title(report):
